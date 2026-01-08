19b3a7dd6b23b96c0758728d797ffa4d4cd40950
Berkay Orhan <berkayorhan@hotmail.se>
2026-01-08 01:30:31 +0100

    feat: integrate Vercel Speed Insights for performance monitoring

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { Geist, Geist_Mono, Inter, Ultra } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
 import { Analytics } from "@vercel/analytics/next"
-
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
@@ -47,6 +46,7 @@ export default function RootLayout({
           >
           {children}
           <Analytics />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
